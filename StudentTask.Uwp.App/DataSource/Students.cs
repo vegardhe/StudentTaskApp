810887b0667a5a747ed79c0be3a284c6502f0c66
Vegard Solberg Hermansen <vegardhe@hiof.no>
2018-03-29 16:46:29 +0200

    Login page is now fullscreen, added task page.

---
+++ StudentTask.Uwp.App/DataSource/Students.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StudentTask.Model;
+
+namespace StudentTask.Uwp.App.DataSource
+{
+    public class Students
+    {
+        public static Students Instance { get; } = new Students();
+
+        private const string BaseUri = "http://localhost:52988/api/";
+
+        private HttpClient _client;
+
+        private Students()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUri)
+            };
+        }
+
+        public async Task<Student> LoginTask(Student student)
+        {
+            var postBody = JsonConvert.SerializeObject(student);
+            var response = await _client
+                .PostAsync("students/login", new StringContent(postBody, Encoding.UTF8, "application/json"))
+                .ConfigureAwait(false);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var seesionStudent = JsonConvert.DeserializeObject<Student>(responseBody);
+            return seesionStudent;
+        }
+    }
+}
--- StudentTask.Uwp.App/Views/LoginPage.xaml.cs
+++ StudentTask.Uwp.App/Views/LoginPage.xaml.cs
@@ -7,11 +7,16 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using StudentTask.Model;
+using Template10.Controls;
+using Template10.Services.NavigationService;
+using Task = System.Threading.Tasks.Task;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +30,33 @@ namespace StudentTask.Uwp.App.Views
         public LoginPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Views.Shell.HamburgerMenu.IsFullScreen = true;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            Views.Shell.HamburgerMenu.IsFullScreen = false;
+        }
+
+        private async void Login(object sender, RoutedEventArgs e)
+        {
+            var student = new Student { Username = UsernameBox.Text, Password = PasswordBox.Password};
+            try
+            {
+                Student loginStudent;
+                if ((loginStudent = await DataSource.Students.Instance.LoginTask(student)) != null)
+                    Frame.Navigate(typeof(TaskPage));
+                else
+                    ErrorBlock.Text = "Invalid username/password.";
+            }
+            catch (Exception ex)
+            {
+                //TODO: Exception handling.
+            }
         }
     }
 }
