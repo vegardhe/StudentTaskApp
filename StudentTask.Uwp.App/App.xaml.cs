c033c506b06cc96890b8006aabd6a124ee3c66bc
Vegard Solberg Hermansen <vegardhe@hiof.no>
2018-03-29 14:49:10 +0200

    Added uwp, base loginpage

---
+++ StudentTask.Uwp.App/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using StudentTask.Uwp.App.Services.SettingsServices;
+using System.Threading.Tasks;
+using Template10.Common;
+using Template10.Controls;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace StudentTask.Uwp.App
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    [Bindable]
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            SplashFactory = (e) => new Views.Splash(e);
+
+            #region app settings
+
+            // some settings must be set in app.constructor
+            var settings = SettingsService.Instance;
+            RequestedTheme = settings.AppTheme;
+            CacheMaxDuration = settings.CacheMaxDuration;
+            ShowShellBackButton = settings.UseShellBackButton;
+
+            #endregion
+        }
+
+        public override UIElement CreateRootElement(IActivatedEventArgs e)
+        {
+            var service = NavigationServiceFactory(BackButton.Attach, ExistingContent.Exclude);
+            return new ModalDialog
+            {
+                DisableBackButtonWhenModal = true,
+                Content = new Views.Shell(service),
+                ModalContent = new Views.Busy(),
+            };
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            // TODO: add your long-running task here
+            await NavigationService.NavigateAsync(typeof(Views.LoginPage));
+        }
+    }
+}
---
+++ StudentTask.Uwp.App/Services/SettingsServices/SettingsService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Template10.Common;
+using Template10.Utils;
+using Windows.UI.Xaml;
+
+namespace StudentTask.Uwp.App.Services.SettingsServices
+{
+    public class SettingsService
+    {
+        public static SettingsService Instance { get; } = new SettingsService();
+        Template10.Services.SettingsService.ISettingsHelper _helper;
+        private SettingsService()
+        {
+            _helper = new Template10.Services.SettingsService.SettingsHelper();
+        }
+
+        public bool UseShellBackButton
+        {
+            get { return _helper.Read(nameof(UseShellBackButton), true); }
+            set
+            {
+                _helper.Write(nameof(UseShellBackButton), value);
+                BootStrapper.Current.NavigationService.GetDispatcherWrapper().Dispatch(() =>
+                {
+                    BootStrapper.Current.ShowShellBackButton = value;
+                    BootStrapper.Current.UpdateShellBackButton();
+                });
+            }
+        }
+
+        public ApplicationTheme AppTheme
+        {
+            get
+            {
+                var theme = ApplicationTheme.Light;
+                var value = _helper.Read(nameof(AppTheme), theme.ToString());
+                return Enum.TryParse(value, out theme) ? theme : ApplicationTheme.Dark;
+            }
+            set
+            {
+                _helper.Write(nameof(AppTheme), value.ToString());
+                ((FrameworkElement) Window.Current.Content).RequestedTheme = value.ToElementTheme();
+                Views.Shell.HamburgerMenu.RefreshStyles(value, true);
+            }
+        }
+
+        public TimeSpan CacheMaxDuration
+        {
+            get { return _helper.Read(nameof(CacheMaxDuration), TimeSpan.FromDays(2)); }
+            set
+            {
+                _helper.Write(nameof(CacheMaxDuration), value);
+                BootStrapper.Current.CacheMaxDuration = value;
+            }
+        }
+
+        public bool ShowHamburgerButton
+        {
+            get { return _helper.Read(nameof(ShowHamburgerButton), true); }
+            set
+            {
+                _helper.Write(nameof(ShowHamburgerButton), value);
+                Views.Shell.HamburgerMenu.HamburgerButtonVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public bool IsFullScreen
+        {
+            get { return _helper.Read(nameof(IsFullScreen), false); }
+            set
+            {
+                _helper.Write(nameof(IsFullScreen), value);
+                Views.Shell.HamburgerMenu.IsFullScreen = value;
+            }
+        }
+    }
+}
